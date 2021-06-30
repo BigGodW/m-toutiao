8225ceeb1361bfa4229ccefa17a4903188ed1cd6
biggodw <biggodw@gmail.com>
2021-06-30 11:38:27 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 移动端适配
+import 'amfe-flexible'
+// 全局样式
+import '@/styles/index.less'
 
 Vue.config.productionTip = false
+Vue.use(Vant)
 
 new Vue({
   router,
